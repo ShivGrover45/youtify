f4a7e9753760ed5404347419efaf52ce171b8ede
ShivGrover45 <codershiv16@gmail.com>
2026-03-12 17:31:21 +0530

    upload music feature added

--- src/controller/music.controller.js
+++ src/controller/music.controller.js
@@ -2,18 +2,41 @@ const musicModel=require('../models/music.model')
 const jwt=require('jsonwebtoken')
 const {uploadFile}=require('../services/storage.service')
 const postMusic=async(req,res)=>{
+    const token=req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:"unauthorized"
+        })
+    }
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        if(decoded.role!=='artist'){
+            return res.status(403).json({
+                message:"Forbidden"
+            })
+        }
+    
     const {title}=req.body
     const file=req.file
-    const result=await uploadFile(file.buffer.toString('base64'))
+    console.log(file)
+    const result= await uploadFile(file.buffer.toString('base64'))
+    console.log(result)
     const music=await musicModel.create({
         uri:result.url,
-        title:title,
-        artist:"ShivGrover"
+        title,
+        artist:decoded.id
     })
     res.status(201).json({
         message:"music created succesfully",
         music
     })
 }
+    catch(err){
+        console.log(err)
+        return res.status(401).json({
+           err
+        })
+    }
+}
 
-module.exports=postMusic
+module.exports={postMusic}
--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,13 +1,14 @@
 const express=require('express')
-const postMusic = require('../controller/music.controller')
+const musicController = require('../controller/music.controller')
 const multer=require('multer')
-const router=express.Router()
+
 const upload=multer({
     storage:multer.memoryStorage()
 })
 
+const router=express.Router()
 
-router.post('/upload',upload.single('music'),postMusic)
+router.post('/upload',upload.single("music"),musicController.postMusic)
 
 
 module.exports=router

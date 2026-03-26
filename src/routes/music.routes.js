6dd70b47e099db529e4883862ff7ef74fb82c0c4
ShivGrover45 <codershiv16@gmail.com>
2026-03-26 12:31:28 +0530

    search feature added

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -15,4 +15,5 @@ router.post('/album',authMiddleware.authArtist,musicController.createAlbum)
 router.get('/',authMiddleware.authUser,musicController.getAllMusics)
 router.get('/albums',authMiddleware.authUser,musicController.getAlbums)
 router.get('/stream/:id',authMiddleware.authUser,musicController.streamMusic)
+router.get('/search',authMiddleware.authUser,musicController.searchMusic)
 module.exports=router

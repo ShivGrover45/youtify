946f2726947aecf2e3ee6990d15acbe2062c5444
ShivGrover45 <codershiv16@gmail.com>
2026-04-26 18:26:56 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 const app=express()
 
 app.use(cors({
-    origin:'https://youtify-frontend.vercel.app/',
+    origin:'https://youtify-frontend.vercel.app',
     credentials:true
 }))
 app.use(express.json())

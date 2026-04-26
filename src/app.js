e7594411d6f3d4d057935d20f4aa387156e808ab
ShivGrover45 <codershiv16@gmail.com>
2026-04-26 13:51:02 +0530

    cors policy updated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const cookie=require('cookie-parser')
 const cors=require('cors')
 const app=express()
 
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookie())
 
